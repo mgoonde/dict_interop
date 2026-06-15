2db9c984613e8572309702276faf975ee0360faf
mgoonde <miha.gunde@gmail.com>
2026-06-15 21:20:03 +0200

    remove old

--- ctest/compile.sh
+++
@@ -1,2 +0,0 @@
-cd ../ && rm -rf bld && cmake -B bld && cmake --build bld && cd -
-gcc -I../include -I../src -g -o ctest.x ctest.c -L../lib -ldb -lgfortran
--- test/compile.sh
+++
@@ -1,3 +0,0 @@
-cd ../ && rm -rf bld && cmake -B bld && cmake --build bld && cd -
-gfortran -g -I../include -o test_dbval.x test_dbval.f90 -L../lib -ldb
-gfortran -g -I../include -o test_db.x test_db.f90 -L../lib -ldb
